9f739fb372ac577a706e4110315ba045a3b4791f
yi yang <61374231+yy995132670@users.noreply.github.com>
2024-10-29 13:17:21 +0800

    Add files via upload

---
+++ defense/__init__.py
@@ -0,0 +1,14 @@
+from .gcn import GCN
+from .gcn_saint import SAINT
+from .r_gcn import RGCN
+from .gcn_attack import GCN_attack
+from .gat import GAT
+from .gin import GIN
+from .gin_kan import GIN_KAN
+from .gcn_preprocess import GCNSVD, GCNJaccard
+from .jumpingknowledge import JK
+
+from .basicfunction import att_coef, accuracy_1
+
+__all__ = ['GCN', 'GCNSVD', 'GCNJaccard', 'RGCN', 'GCN_attack','GAT', 'GIN', 'GIN_KAN', 'att_coef', 'accuracy_1', 'JK',
+          'SAINT']
